f5064d1eb8335fe884ff9344c09d6987e269d47b
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-05-08 18:34:26 -0300

    moved frontend to frontend folder

--- grow/api/routes/productComment.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const commentController = require('../controllers/commentController');
-
-router.post('/add',commentController.addComment);
-router.get('/:productId',commentController.getAllComments); 
-
-module.exports=router;
--- grow/api/routes/shoppingCart.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const shoppingCartController = require('../controllers/shoppingCartController');
-
-router.get('/:id', shoppingCartController.get);
-router.put('/total', shoppingCartController.update);
-router.delete('/:id', shoppingCartController.destroy);
-
-module.exports = router;
