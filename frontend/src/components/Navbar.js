fed1d010fded60cee6bafa81e9a6eaddd5deb673
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-05-10 12:23:13 -0300

    Updated Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Offcanvas,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
-
-import { Sidebar } from "./Sidebar";
-import { Login } from "./Login";
 import "../style/Navbar.css";
-import { useSelector } from "react-redux";
-import useInput from "../hooks/useInput";
+import UserDropdown from "./UserDropdown";
+// import Sidebar from "./Sidebar-backup";
+import Sidebar from "./Sidebar";
 import axios from "axios";
+import useInput from "../hooks/useInput";
 
-const Navbar = ({ setProducts }) => {
-  const user = useSelector((state) => state.user);
+const NavbarComp = ({ setProducts }) => {
+  const [visibleSearch, setVisibleSearch] = useState(false);
   const searchValue = useInput();
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,187 +28,85 @@ const Navbar = ({ setProducts }) => {
         .then((res) => setProducts(res.data));
     }
   };
-
+  const handleVisible = () => {
+    setVisibleSearch(!visibleSearch);
+  };
   return (
-    <div className="shadow" style={{ backgroundColor: "#f8f9fa" }}>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link to="/" className="navbar-brand title">
-            Sativa
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <div className="dropdown1">
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    GROWSHOP
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <li>
-                      <Link to={`/products/popular`}>
-                        <a className="dropdown-item" href="#">
-                          Productos Populares
-                        </a>
-                      </Link>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        catalogo 2
-                      </a>
-                    </li>
-                  </ul>
-                </li>
+    <>
+      {/* <Container fluid> */}
+      <div className="navbar-container">
+        <Link to="/" className="title">
+          GreenShop
+        </Link>
 
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
+        <Navbar bg="light" expand={"lg"} style={{ flex: 1 }}>
+          <Container fluid style={{ paddingRight: "0" }}>
+            <Navbar.Toggle
+              aria-controls={`offcanvasNavbar-expand-${"lg"}`}
+              style={{ position: "absolute", marginLeft: "10px" }}
+            />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${"lg"}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${"lg"}`}
+              placement="start"
+            >
+              <Offcanvas.Header closeButton>
+                {/* <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${"lg"}`}>
+                  Menu
+                </Offcanvas.Title> */}
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav
+                  className="justify-content-center flex-grow-1 pe-3 centered-nav"
+                  // style={{ alignItems: "center" }} poner en breakpoint porque en columna se centra
+                >
+                  <Link
+                    to={`/products/popular`}
+                    className="nav-link"
+                    style={{ textDecoration: "none" }}
                   >
-                    COMBOS
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
+                    Products
+                  </Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <NavDropdown
+                    title="Categories"
+                    id={`offcanvasNavbarDropdown-expand-${"lg"}`}
                   >
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        combo invierno
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        combo verano
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
+                    <NavDropdown.Item href="#action3">Plants</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Fertilizers
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <form className="d-flex" onSubmit={handleSubmit}>
+                  {visibleSearch && (
+                    <input
+                      className="form-control me-2 search-input"
+                      type="search"
+                      placeholder="Search"
+                      aria-label="Search"
+                      {...searchValue}
+                    />
+                  )}
+                  <Button
+                    className="iconNavbar"
+                    type="submit"
+                    variant="success"
+                    onClick={handleVisible}
                   >
-                    NOSOTROS
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        nuestra historia
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        nuestra mision
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-
-                {user.roleId === 2 || user.roleId === 3 ? (
-                  <li className="nav-item dropdown">
-                    <a
-                      className="nav-link dropdown-toggle"
-                      href="#"
-                      id="adminDropdown"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                      style={{ backgroundColor: "#008200", color: "white" }}
-                    >
-                      ADMIN
-                    </a>
-                    <ul
-                      className="dropdown-menu"
-                      aria-labelledby="navbarDropdown"
-                    >
-                      <li>
-                        <Link to={`/admin/users`} className="dropdown-item">
-                          Usuarios
-                        </Link>
-                      </li>
-
-                      <li>
-                        <Link to={`/admin/orders`} className="dropdown-item">
-                          Órdenes
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to={`/admin/products`} className="dropdown-item">
-                          Productos
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to={`/admin/categories`}
-                          className="dropdown-item"
-                        >
-                          Categorías
-                        </Link>
-                      </li>
-                    </ul>
-                  </li>
-                ) : (
-                  <></>
-                )}
-              </div>
-            </ul>
-            <form className="d-flex" onSubmit={handleSubmit}>
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                {...searchValue}
-              />
-              <a className="iconNavbar" type="submit">
-                <ion-icon name="search-outline"></ion-icon>
-              </a>
-            </form>
-          </div>
-          {user.id ? (
-            <div>
-              <span>{user.name}</span>
-            </div>
-          ) : null}
-        </div>
-      </nav>
-
-      <Login />
-      <Sidebar />
-    </div>
+                    <ion-icon name="search-outline"></ion-icon>
+                  </Button>
+                </form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+        <Sidebar />
+        <UserDropdown />
+      </div>
+      {/* </Container> */}
+    </>
   );
 };
 
-export default Navbar;
+export default NavbarComp;
