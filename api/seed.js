306c50cb6e4d3fcfc44a56bd58a51add7cca96c0
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-05-18 13:47:43 -0300

    Updated seed to create roles and categories

--- api/seed.js
+++ api/seed.js
@@ -2,6 +2,8 @@ const fakeData = require("./Product.json");
 const db = require("./db");
 const { Product } = require("./models");
 const { Role } = require("./models");
+const { CategoryFamily } = require("./models");
+const { Category } = require("./models");
 
 const setupSeed = () => {
   const fake = [];
@@ -9,11 +11,42 @@ const setupSeed = () => {
     fake.push(fakeData.productos[i]);
   }
   const admin = [{ role: "Client" }, { role: "Admin" }, { role: "SuperAdmin" }];
-  Product.bulkCreate(fake)
-    .then((product) => console.log(product))
-    .then(() => {
-      Role.bulkCreate(admin).then((roles) => console.log(roles));
-    });
+  const categoryFamilies = [
+    {
+      name: "Plants",
+      description: "Green Living beigns",
+    },
+    {
+      name: "Accesories",
+      description: "Accesories to take care of plants",
+    },
+  ];
+  const categories = [
+    {
+      name: "All Plants",
+      description: "All our green living beings",
+      categoryfamilyId: "1",
+    },
+    {
+      name: "All Accesories",
+      description: "The best plant accesories",
+      categoryfamilyId: "2",
+    },
+    {
+      name: "House Plants",
+      description: "The green homies",
+      categoryfamilyId: "1",
+    },
+    {
+      name: "Fertilizers",
+      description: "Plants food",
+      categoryfamilyId: "2",
+    },
+  ];
+
+  return Role.bulkCreate(admin)
+    .then((roles) => CategoryFamily.bulkCreate(categoryFamilies))
+    .then((categoryFamilies) => Category.bulkCreate(categories));
 };
 
 db.sync().then(setupSeed);
