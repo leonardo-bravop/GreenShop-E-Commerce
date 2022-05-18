6ecd77669f05fea7e24081668296f667b23a4538
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-05-18 14:00:37 -0300

    created inputs and prices utils

--- frontend/src/utils/InputValidations.js
+++ frontend/src/utils/InputValidations.js
@@ -2,7 +2,7 @@ exports.validateEmail = (email) => {
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
     return true;
   }
-  alert("You have entered an invalid email address!");
+  alert("Please enter a valid email address");
   return false;
 };
 
@@ -10,6 +10,6 @@ exports.validateString = (str) => {
   if (/^[a-z ,.'-]+$/i.test(str)) {
     return true;
   }
-  alert("Your name or lastname is not valid!");
+  alert("Please enter a valid name or lastname");
   return false;
 };
