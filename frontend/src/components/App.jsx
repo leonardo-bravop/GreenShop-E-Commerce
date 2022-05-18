8397ca09136f1488ed5084f3dfba1733bf934b82
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-05-18 13:59:10 -0300

    created home component and created routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,13 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router";
 import axios from "axios";
-import Grid from "./Grid";
-import CarouselComponent from "./Carousel";
 // import Navbar from './Navbar-backup';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { persistUser } from "../state/user";
 import { getShoppingCart } from "../state/shoppingCart";
+import { getItemCart } from "../state/itemCart";
 
 import SingleProduct from "../commons/SingleProduct.jsx";
 import AdminUsers from "./AdminUsers";
@@ -17,11 +16,11 @@ import AdminOrders from "./AdminOrders";
 import AdminProducts from "./AdminProducts";
 import NewProductForm from "./NewProductForm";
 import EditProductForm from "./EditProductForm";
-import { getItemCart } from "../state/itemCart";
 import OrderHistorial from "./OrderHistorial";
 import CartDetails from "./CartDetails";
 import FilterSearch from "./FilterSearch";
 import NotFound from "./NotFound";
+import FilterSearchCategory from "./FilterSearchCategory";
 
 import AdminCategories from "./AdminCategories";
 import NewCategForm from "./NewCategForm";
@@ -29,6 +28,7 @@ import EditCategForm from "./EditCategForm";
 
 import OrderDetails from "./OrderDetail";
 import Home from "./Home";
+import ValueSearch from "./ValueSearch";
 
 function App() {
   const dispatch = useDispatch();
@@ -41,57 +41,88 @@ function App() {
   }, []);
 
   useEffect(() => {
-    dispatch(getShoppingCart()).then(() => dispatch(getItemCart()));
+    dispatch(getShoppingCart()).then(({ payload }) => {
+      if (payload) dispatch(getItemCart(payload.id));
+    });
   }, [user.id]);
 
   const [products, setProducts] = useState([]);
+  const [categoryFamilies, setCategoryFamilies] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/product/").then((res) => setProducts(res.data));
+    axios
+      .get("/api/product/")
+      .then((res) => {
+        setProducts(res.data);
+        return axios.get("/api/categoryFamily/getAll");
+      })
+      .then(({ data }) => {
+        setCategoryFamilies(data);
+      });
   }, []);
 
   return (
-    <div className="App">
-      <Navbar setProducts={setProducts} />
-      {/* <div className="container"> */}
-      <Routes>
-        <Route path="/" element={<Home products={products} />} />
-        <Route
-          path="/products/popular"
-          element={<FilterSearch products={products} />}
-        />
-        <Route path="/orders/history" element={<OrderHistorial />} />
-        <Route path="/CartDetails" element={<CartDetails />} />
-        <Route path="/orderDetails/:id" element={<OrderDetails />} />
+    <div
+      className="App"
+      style={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <div>
+        <Navbar setProducts={setProducts} />
+        {/* <div className="container"> */}
+        <Routes>
+          <Route path="/" element={<Home products={products} />} />
+          <Route
+            path="/products/all"
+            element={<FilterSearch products={products} />}
+          />
+          {categoryFamilies.map((family) => {
+            return (
+              <Route
+                path={`/${family.name}/:name`}
+                element={<FilterSearchCategory family={family} />}
+                key={family.id}
+              />
+            );
+          })}
+          <Route path="/orders/history" element={<OrderHistorial />} />
+          <Route path="/CartDetails" element={<CartDetails />} />
+          <Route path="/orderDetails/:id" element={<OrderDetails />} />
 
-        <Route path="/product/:id" element={<SingleProduct />} />
-        {user.roleId === 2 ? (
-          <>
-            <Route path="/admin/users" element={<AdminUsers />} />
-            <Route path="/admin/orders" element={<AdminOrders />} />
-            <Route path="/admin/products" element={<AdminProducts />} />
-            <Route
-              path="/admin/products/new-product"
-              element={<NewProductForm />}
-            />
-            <Route
-              path="/admin/products/edit/:id"
-              element={<EditProductForm />}
-            />
-            <Route path="/admin/categories" element={<AdminCategories />} />
-            <Route
-              path="/admin/categories/new-category"
-              element={<NewCategForm />}
-            />
-            <Route
-              path="/admin/categories/edit/:id"
-              element={<EditCategForm />}
-            />
-          </>
-        ) : null}
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-      {/* </div> */}
+          <Route path="/product/:id" element={<SingleProduct />} />
+          <Route path="/search/value/:value" element={<ValueSearch />} />
+          {user.roleId === 2 ? (
+            <>
+              <Route path="/admin/users" element={<AdminUsers />} />
+              <Route path="/admin/orders" element={<AdminOrders />} />
+              <Route path="/admin/products" element={<AdminProducts />} />
+              <Route
+                path="/admin/products/new-product"
+                element={<NewProductForm />}
+              />
+              <Route
+                path="/admin/products/edit/:id"
+                element={<EditProductForm />}
+              />
+              <Route path="/admin/categories" element={<AdminCategories />} />
+              <Route
+                path="/admin/categories/new-category"
+                element={<NewCategForm />}
+              />
+              <Route
+                path="/admin/categories/edit/:id"
+                element={<EditCategForm />}
+              />
+            </>
+          ) : null}
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        {/* </div> */}
+      </div>
       <Footer />
     </div>
   );
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,32 +1,37 @@
 import { Container } from "react-bootstrap";
 import CategoryCard from "../commons/CategoryCard";
+import ProductsRow from "../commons/ProductsRow";
 import Grid from "./Grid";
 
-const Home = ({ products }) => {
+const Home = () => {
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "30vh",
-          width: "100%",
-          backgroundPositionY: "80%",
-          // backgroundPositionX: "500px",
-          // backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundImage:
-            // "url(https://images.unsplash.com/photo-1501004318641-b39e6451bec6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80)"
-            "url(https://images.unsplash.com/photo-1504541891213-1b1dfdadb739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)",
-          // "url(https://images.unsplash.com/photo-1457530378978-8bac673b8062?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)",
-          filter: "grayscale: 80%",
-        }}
-      >
+      <div style={{display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "30vh",
+            width: "100%",}}>
+        <div
+          style={{
+          
+            height: "100%",
+            width: "100%",
+            backgroundPositionY: "80%",
+            // backgroundPositionX: "500px",
+            // backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundImage:
+              // "url(https://images.unsplash.com/photo-1501004318641-b39e6451bec6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80)"
+              "url(https://images.unsplash.com/photo-1504541891213-1b1dfdadb739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)",
+            // "url(https://images.unsplash.com/photo-1457530378978-8bac673b8062?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)",
+            filter: "brightness(70%)",
+          }}
+        ></div>
         <h1
           style={{
             padding: "10px",
-            backgroundColor: "rgba(0,0,0,0.4)",
+            position: "absolute",
+            // backgroundColor: "rgba(0,0,0,0.4)",
             color: "white",
           }}
         >
@@ -34,13 +39,26 @@ const Home = ({ products }) => {
         </h1>
       </div>
       {/* <CarouselComponent /> */}
-      <Container style={{marginTop: "30px"}}>
+      <Container style={{ marginTop: "30px" }}>
         <h2>Shop by category</h2>
-          <CategoryCard/>
+        <CategoryCard />
       </Container>
-      <div className="container">
-        <Grid products={products} />
+
+      <div className="largetext-wrapper">
+        <div
+          style={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1520302630591-fd1c66edc19d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)",
+          }}
+          className="largetext-container"
+        ></div>
+        <div className="largetext-title">All your plants need in one place</div>
       </div>
+
+      <Container style={{marginTop: "30px"}}>
+        <h2>Latest</h2>
+        <ProductsRow />
+      </Container>
     </>
   );
 };
