7adb8338b7adf21fd28d3971962554c1942c688e
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-05-18 13:54:05 -0300

    updated comments and valoration

--- frontend/src/commons/Valoration.js
+++ frontend/src/commons/Valoration.js
@@ -3,129 +3,42 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import axios from "axios";
 import "../style/Valoration.css";
+import { Button, Modal } from "react-bootstrap";
+// import Comments from "../components/Comments";
+import useInput from "../hooks/useInput";
+import ValorationModal from "./ValorationModal.js";
 
-const Valoration = ({ valoration }) => {
+const Valoration = ({ setAverageValoration, setComments }) => {
   const user = useSelector((state) => state.user);
   const { id } = useParams();
-
-  const [start1, setStart1] = useState(false);
-  const [start2, setStart2] = useState(false);
-  const [start3, setStart3] = useState(false);
-  const [start4, setStart4] = useState(false);
-  const [start5, setStart5] = useState(false);
-
-  const startA = () => {
-    setStart1(true);
-    setStart2(false);
-    setStart3(false);
-    setStart4(false);
-    setStart5(false);
-  };
-  const startB = () => {
-    setStart1(true);
-    setStart2(true);
-    setStart3(false);
-    setStart4(false);
-    setStart5(false);
-  };
-  const startC = () => {
-    setStart1(true);
-    setStart2(true);
-    setStart3(true);
-    setStart4(false);
-    setStart5(false);
-  };
-  const startD = () => {
-    setStart1(true);
-    setStart2(true);
-    setStart3(true);
-    setStart4(true);
-    setStart5(false);
-  };
-  const startE = () => {
-    setStart1(true);
-    setStart2(true);
-    setStart3(true);
-    setStart4(true);
-    setStart5(true);
-  };
+  const [userValoration, setUserValoration] = useState(0);
 
   useEffect(() => {
-    axios
-      .get(`/api/productValoration/getAll/${id}`)
-
-      .then((res) => {
-        console.log("respuesta de valoracion---->", res);
-
-        if (res.data.length > 0) {
-            let valoracion = 0;
-            res.data.map(item=> valoracion += item.valoration) ;
-            const prom = valoracion/res.data.length ;
-            console.log("soy tu prom",valoracion) ;
-            valoration(prom.toFixed(1));
-        } 
-      });
+    axios.get(`/api/productValoration/getAverage/${id}`).then(({ data }) => {
+      if (data) {
+        setAverageValoration(data.average);
+      }
+    });
   }, []);
-  const click = (valoration) => {
-    axios
-      .post(`/api/productValoration/add/${id}`, {
-        valoration,
-        userId: user.id,
-      })
-      .then(() =>{
-        return axios
-          .get(`/api/productValoration/getAll/${id}`)
 
-          .then((res) => {
-            if (res.data){
-                let valoracion = 0;
-                res.data.map(item=> valoracion += item.valoration) ;
-                const prom = valoracion/res.data.length ;
-                console.log("soy tu prom",valoracion) ;
-               return  valoration(prom.toFixed(1));
-            } 
-          })
-        })
-      .catch((err) => console.log(err));
-  };
+  useEffect(() => {
+    if (user.id) {
+      axios
+        .get(`/api/productValoration/getByUserId/${user.id}/productId/${id}`)
+        .then(({ data }) => {
+          setUserValoration(data.valoration);
+        });
+    }
+  }, [user.id]);
 
   return (
-    <div className="starts">
-      <button
-        className={start1 ? "start1" : "start11"}
-        onMouseOver={() => startA()}
-        onClick={() => click(1)}
-      >
-        <ion-icon name="star-sharp"></ion-icon>
-      </button>
-      <button
-        className={start2 ? "start2" : "start22"}
-        onMouseOver={() => startB()}
-        onClick={() => click(2)}
-      >
-        <ion-icon name="star-sharp"></ion-icon>
-      </button>
-      <button
-        className={start3 ? "start3" : "start33"}
-        onMouseOver={() => startC()}
-        onClick={() => click(3)}
-      >
-        <ion-icon name="star-sharp"></ion-icon>
-      </button>
-      <button
-        className={start4 ? "start4" : "start44"}
-        onMouseOver={() => startD()}
-        onClick={() => click(4)}
-      >
-        <ion-icon name="star-sharp"></ion-icon>
-      </button>
-      <button
-        className={start5 ? "start5" : "start55"}
-        onMouseOver={() => startE()}
-        onClick={() => click(5)}
-      >
-        <ion-icon name="star-sharp"></ion-icon>
-      </button>
+    <div>
+      <ValorationModal
+        userValoration={userValoration}
+        setUserValoration={setUserValoration}
+        setComments={setComments}
+        setAverageValoration={setAverageValoration}
+      />
     </div>
   );
 };
--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -1,77 +1,49 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { useSelector } from 'react-redux';
-import useInput from '../hooks/useInput';
-import '../style/Comments.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { useSelector } from "react-redux";
+import useInput from "../hooks/useInput";
+import "../style/Comments.css";
+import { Button, Modal } from "react-bootstrap";
+import Valoration from "../commons/Valoration";
 
-const Comments = () => {
+const Comments = ({ comments, setComments }) => {
+  const user = useSelector((state) => state.user);
   const { id } = useParams();
-  const [comments, setComments] = useState([]);
-  const comment = useInput('');
-  const user = useSelector(state => state.user);
-  useEffect(() => {
-    axios.get(`/api/productComment/${id}`).then(res => {
-      console.log('SOY EL RES------------>', res);
+  const comment = useInput("");
 
-      if (res.data) setComments(res);
-    });
-  }, []);
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (user.id && comment.value) {
+  //     axios
+  //       .post(`/api/productValoration/add`, {
+  //         UserId: user.id,
+  //         review: comment.value,
+  //         productId: id,
+  //         valoration, 
+  //       })
+  //       .then(() => {
+  //         document.querySelector(".formComments").value = "";
+  //         comment.onChange(e);
+  //         return axios.get(`/api/productComment/${id}`);
+  //       })
+  //       .then((res) => setComments(res.data));
+  //   }
+  // };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (user.id) {
-      axios
-        .post(`/api/productComment/add`, {
-          userName: user.name,
-          userId: user.id,
-          comment: comment.value,
-          productId: id,
-        })
-        .then(() => {
-          axios.get(`/api/productComment/${id}`).then(res => setComments(res));
-        });
-    }
-  };
-  console.log('MIS COMMENTS ', comments);
+ 
   return (
-    <div className=''>
-      <div className='titleComment'>Tu comentario nos ayuda a mejorar</div>
-      <form onSubmit={handleSubmit} className='formComments'>
-        <input className='inputComment' {...comment} />
-        {user.id ? (
-          <button className='buttonComment' type='submit'>
-            Comentar
-          </button>
-        ) : (
-          <>
-            <button
-              class='buttonComment'
-              type='button'
-              data-bs-toggle='collapse'
-              data-bs-target='#collapseExample'
-              aria-expanded='false'
-              aria-controls='collapseExample'
-            >
-              Comentar
-            </button>
-            <div class='collapse' id='collapseExample'>
-              <div class='card-body'>
-                Necesitas iniciar sesion pra poder comentar
-              </div>
-            </div>
-          </>
-        )}
-      </form>
-      <div>
-        {comments?.data?.map(({ userId, userName, comment }) => (
-          <div key={userId} className='comment'>
-            <h className='userComment'>{userName}</h>
-            <p>{comment}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+
+    <>
+     <div>Add a comment:</div>
+            {/* <form onSubmit={handleSubmit} className="formComments">
+              <input className="inputComment" {...comment} />
+              <button className="buttonComment" type="submit">
+                Comentar
+              </button>
+            </form> */}
+          
+    </>
   );
 };
 export default Comments;
