81893f0d62bb47358d82b5ea1e3b35f871d29151
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-05-18 13:54:58 -0300

    updated shopping cart style and state

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Button, Offcanvas } from "react-bootstrap";
+import { Badge, Button, Offcanvas } from "react-bootstrap";
 
 import Cart from "./Cart";
-import "../style/Sidebar.css"
+import "../style/Sidebar.css";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
   const [showSidebar, setShowSideBar] = useState(false);
@@ -10,21 +11,30 @@ const Sidebar = () => {
   const handleClose = () => setShowSideBar(false);
   const handleShow = () => setShowSideBar(true);
 
+  const cartItems = useSelector((state) => state.itemCarts);
+
   return (
     <div className="cart-nav-div">
-      <Button variant="secondary" onClick={handleShow}>
+      <Button
+        variant="secondary"
+        onClick={handleShow}
+        style={{ position: "relative" }}
+      >
         <ion-icon name="cart-outline"></ion-icon>
+
+        <span style={{ position: "absolute", left: "30px", bottom: "20px" }}>
+          <Badge bg={cartItems.length? "success" : "secondary"}>{cartItems.length}</Badge>
+        </span>
       </Button>
 
       <Offcanvas show={showSidebar} onHide={handleClose} placement="end">
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Carrito de compras</Offcanvas.Title>
+          <Offcanvas.Title>Shopping Cart</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-         <Cart/>
+          <Cart setShowSideBar={setShowSideBar}/>
         </Offcanvas.Body>
       </Offcanvas>
-    
     </div>
   );
 };
