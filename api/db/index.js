3b95375f3b72d6c89a25f66b11dc10effea6cfc2
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-05-19 12:27:41 -0300

    updated database config

--- api/db/index.js
+++ api/db/index.js
@@ -1,21 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config()
 
-const db = new Sequelize('greenshop', null, null, {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false,
-});
-
-// const db = new Sequelize(process.env.DATABASE_URL,
-//   {
-//     dialect: "postgres",
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   });
+const db = new Sequelize(process.env.DATABASE_URL,
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 
 module.exports = db;
